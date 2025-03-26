cbf60fe1bfbe3bc6531b3e0de3bd358fc194b096
JoeylMS <ivbazulyk@gmail.com>
2025-03-26 18:02:09 +0200

    setting start

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
 export default function Home() {
     return (
-        <h1 className="text-3xl font-bold text-blue-400 underline">
+        <h1 className="text-3xl bg-bermuda text-midnight font-bold underline">
             Hello world!
         </h1>
     );
