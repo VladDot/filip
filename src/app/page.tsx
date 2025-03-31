c2ca28bccc433c146bb6a0aadc6278e077a3f331
Ihor Bazulyk <105243926+JoeYlMS@users.noreply.github.com>
2025-03-31 16:54:29 +0300

    added Bg (#14)

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -14,7 +14,7 @@ export default function Home() {
         <div>
             <Header />
             <Banner />
-            <div className="px-5 xl:pl-[60px] xl:pr-12 xxl:m-auto pt-[108px] lg:pt-[200px]">
+            <div className="px-5 xl:pl-[60px] xl:pr-12 xxl:m-auto pt-[108px] lg:pt-[200px] cubeBg md:.cubeBgMd lg:.cubeBgXl">
                 <TargetAudience />
                 <LearningProcess />
                 <CareerDevelopment />
