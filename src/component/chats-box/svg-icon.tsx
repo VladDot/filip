0f4925e8e8fc729b71ed066503ff6693e9d14282
Ihor Bazulyk <105243926+JoeYlMS@users.noreply.github.com>
2025-04-07 19:30:48 +0300

    Fixed style (#24)
    * fixed style
    
    * fixed svg

--- src/component/chats-box/svg-icon.tsx
+++ src/component/chats-box/svg-icon.tsx
@@ -1,12 +1,12 @@
 export const SVGChat = () => {
     return <svg width="60" height="60" viewBox="0 0 60 60" fill="none" xmlns="http://www.w3.org/2000/svg">
     <rect width="60" height="60" rx="2" fill="#027DFA"/>
-    <path d="M37.9998 28.0003H38.2004M30.0001 28.0003H30.1998M21.7998 28.0003H22.0004" stroke="white" stroke-width="3" stroke-linecap="round" stroke-linejoin="round"/>
+    <path d="M37.9998 28.0003H38.2004M30.0001 28.0003H30.1998M21.7998 28.0003H22.0004" stroke="white" strokeWidth="3" strokeLinecap="round" strokeLinejoin="round"/>
   
-    <path d="M43.6205 38.4605L46.0003 48L36.0998 43.0397C34.1297 43.6787 32.0711 44.0026 30 43.9997C19.9997 43.9997 12 36.84 12 28.0003C12 19.1606 19.9997 12 30 12C40.0003 12 48 19.1597 48 28.0003C47.9675 31.926 46.3948 35.6829 43.6205 38.4605Z" stroke="white" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+    <path d="M43.6205 38.4605L46.0003 48L36.0998 43.0397C34.1297 43.6787 32.0711 44.0026 30 43.9997C19.9997 43.9997 12 36.84 12 28.0003C12 19.1606 19.9997 12 30 12C40.0003 12 48 19.1597 48 28.0003C47.9675 31.926 46.3948 35.6829 43.6205 38.4605Z" stroke="white" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/>
     <defs>
-    <filter id="filter0_f_13_828" x="8.5" y="8.5" width="43" height="44.3026" filterUnits="userSpaceOnUse" color-interpolation-filters="sRGB">
-    <feFlood flood-opacity="0" result="BackgroundImageFix"/>
+    <filter id="filter0_f_13_828" x="8.5" y="8.5" width="43" height="44.3026" filterUnits="userSpaceOnUse" colorInterpolationFilters="sRGB">
+    <feFlood floodOpacity="0" result="BackgroundImageFix"/>
     <feBlend mode="normal" in="SourceGraphic" in2="BackgroundImageFix" result="shape"/>
     <feGaussianBlur stdDeviation="1" result="effect1_foregroundBlur_13_828"/>
     </filter>
@@ -18,10 +18,10 @@ export const SVGChat = () => {
     return<svg width="60" height="60" viewBox="0 0 60 60" fill="none" xmlns="http://www.w3.org/2000/svg">
     <path d="M0 2C0 0.895432 0.895431 0 2 0H58C59.1046 0 60 0.895431 60 2V58C60 59.1046 59.1046 60 58 60H2C0.895432 60 0 59.1046 0 58V2Z" fill="#027DFA"/>
     <path d="M34.3452 33.5385L30.0035 29.1927L25.6618 33.5385L24.6924 32.5697L29.0409 28.2308L24.6924 23.8919L25.6618 22.9231L30.0035 27.2689L34.3452 22.9299L35.3078 23.8919L30.966 28.2308L35.3078 32.5697L34.3452 33.5385Z" fill="white"/>
-    <path d="M47.4039 40.8106L50.4449 53L37.7942 46.6618C35.2769 47.4783 32.6464 47.8923 30 47.8885C17.2218 47.8885 7 38.74 7 27.4449C7 16.1497 17.2218 7 30 7C42.7782 7 53 16.1485 53 27.4449C52.9584 32.461 50.9489 37.2614 47.4039 40.8106Z" stroke="white" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+    <path d="M47.4039 40.8106L50.4449 53L37.7942 46.6618C35.2769 47.4783 32.6464 47.8923 30 47.8885C17.2218 47.8885 7 38.74 7 27.4449C7 16.1497 17.2218 7 30 7C42.7782 7 53 16.1485 53 27.4449C52.9584 32.461 50.9489 37.2614 47.4039 40.8106Z" stroke="white" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/>
     <defs>
-    <filter id="filter0_f_13_880" x="3.5" y="3.5" width="53" height="54.3026" filterUnits="userSpaceOnUse" color-interpolation-filters="sRGB">
-    <feFlood flood-opacity="0" result="BackgroundImageFix"/>
+    <filter id="filter0_f_13_880" x="3.5" y="3.5" width="53" height="54.3026" filterUnits="userSpaceOnUse" colorInterpolationFilters="sRGB">
+    <feFlood floodOpacity="0" result="BackgroundImageFix"/>
     <feBlend mode="normal" in="SourceGraphic" in2="BackgroundImageFix" result="shape"/>
     <feGaussianBlur stdDeviation="1" result="effect1_foregroundBlur_13_880"/>
     </filter>
