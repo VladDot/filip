5916ec05006099d144e29cafb56929c41d514fff
Ihor Bazulyk <105243926+JoeYlMS@users.noreply.github.com>
2025-04-08 17:50:29 +0300

    Fixed style (#28)
    * fixed style
    
    * fixed svg
    
    * fixed style Change scroll
    
    * fixed scroll
    
    * fixed style & svg
    
    * fixed style Chat

--- src/component/chats-box/index.tsx
+++ src/component/chats-box/index.tsx
@@ -28,12 +28,12 @@ export const Chat = ({className}:IChatProps) => {
   <div 
   ref={ref}
   onClick={handleClick}
-   className={cn("w-[60px] h-[60px] right-5 bottom-18 z-[1000] fixed transition-all duration-300 ease-in-out",className)}> 
+   className={cn("w-[60px] h-[60px] right-5 bottom-18 z-[1000] fixed transition-all duration-300 ease-in-out bg-blueBg",className)}> 
     <div className={cn("absolute top-0 left-1/2 -translate-x-1/2 overflow-hidden -translate-y-1/2 h-0 duration-150 ease-in-out", isOpen && "-top-[110%] h-fit overflow-visible -translate-y-18")}>
       <Social className="flex-col text-textBlue/80 md:text-[#D5D5D5] md:[&_*]:hover:text-textBlue duration-300 ease-in-out transition-all"/>
     </div>
 
-    <div className={cn("relative z-20 w-[60px] h-[60px] overflow-hidden",isOpen === false && 'hover:[&_*]:scale-110 hover:[&_*]:-translate-x-[3%] hover:[&_*]:-translate-y-[2.5%]')}>
+    <div className={cn("relative z-20 w-[60px] h-[60px] transition-all duration-500 ",isOpen === false && '[&_*]:scale-90 [&_*]:transition-all [&_*]:duration-300 hover:[&_*]:scale-100 [&_*]:translate-x-[2.5%] [&_*]:translate-y-[2.5%] hover:[&_*]:translate-x-0 hover:[&_*]:translate-y-0')}>
       {!isOpen ? <SVGChat/>: <SVGChatClose/>}
 
     </div>
