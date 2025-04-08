a8f67a4c44fb391a7d8375f39c1f7993f0e320cc
VladDot <107644242+VladDot@users.noreply.github.com>
2025-04-08 18:58:14 +0300

    Create/animation box (#29)
    * added squad animation
    
    * added animated
    
    ---------
    
    Co-authored-by: ASGladchenko <asgladchenko@gmail.com>
    Co-authored-by: Vlad <vladkostirya@gmail.com>

--- src/component/career-development/index.tsx
+++ src/component/career-development/index.tsx
@@ -1,26 +1,30 @@
-import { Title } from "../title";
-import { cardCareer } from "./mock";
-import { SubTitle } from "../sub-title";
-import { CardsCareer } from "./cards-career";
+import { Title } from '../title';
+import { cardCareer } from './mock';
+import { SubTitle } from '../sub-title';
+import { CardsCareer } from './cards-career';
 
 export const CareerDevelopment = () => {
-    return (
-        <section className="mb-20 lg:mb-[120px] xxl:mb-[170px] relative z-[4]">
-<div className='absolute z-[1] h-[15%]  lg:h-[45%] bg-white rounded-full blur-[40px] lg:translate-y-[90%] bottom-0 translate-y-[60%] lg:bottom-0 w-full'/>
+  return (
+    <div className="relative">
+      <div className="m-auto mb-[30px] max-w-[650px] text-center md:mb-[60px]">
+        <SubTitle text="Мрія, яка стане реальністю" />
+        <Title text="Кар'єрне зростання IT-фахівця" className="leading-[100%] lg:max-w-[500px]" />
+      </div>
 
-            <div className='absolute z-[4] w-[90vw] h-[80vw] rounded-full bg-[#027DFA80] blur-[100px] right-0 translate-x-[30%] top-0 translate-y-[30%] lg:blur-[200px]  xxl:blur-[200px]   sm:translate-x-[50%]  sm:translate-y-[10%] sm:blur-[180px] lg:translate-x-[70%] xxl:-translate-y-[20%] xxl:translate-x-[80%]'/>
+      <div className="xxl:blur-[200px] xxl:-translate-y-[20%] xxl:translate-x-[80%] absolute top-0 right-0 z-[4] h-[80vw] w-[90vw] translate-x-[30%] translate-y-[30%] rounded-full bg-[#027DFA80] blur-[100px] sm:translate-x-[50%] sm:translate-y-[10%] sm:blur-[180px] lg:translate-x-[70%] lg:blur-[200px]" />
 
-<div className='absolute z-[4] w-[clamp(200px,50vw,1382px)] h-[clamp(200px,50vw,1000px)] rounded-full bg-[#027DFA80] blur-[100px] left-0 top-2/5 -translate-y-[50%] -translate-x-[70%]  md:-translate-y-[30%] lg:blur-[150px] lg:-translate-y-[20%] xxl:-translate-x-[60%] xxl:blur-[150px]  xxl:-translate-y-[40%] lg:-translate-x-[60%]'/>
-            <div className=" max-w-[500px] md:mb-[60px] lg:mb-20 xxl:mb-[100px] m-auto text-center mb-[43px] ">
-                <SubTitle text="Мрія, яка стане реальністю" />
-                <Title
-                    text="Кар'єрне зростання IT-фахівця"
-                    className="leading-[100%]"
-                />
-            </div>
-            <div className="flex bg-white justify-center [&>div>div:first-child>h3]:text-black [&>div>div:first-child>div]:bg-white [&>div>div:first-child>div]:text-textBlue">
-                <CardsCareer cardCareer={cardCareer} />
-            </div>
-        </section>
-    );
+      <div className="xxl:-translate-x-[60%] xxl:blur-[150px] xxl:-translate-y-[40%] absolute top-2/5 left-0 z-[4] h-[clamp(200px,50vw,1000px)] w-[clamp(200px,50vw,1382px)] -translate-x-[70%] -translate-y-[50%] rounded-full bg-[#027DFA80] blur-[100px] md:-translate-y-[30%] lg:-translate-x-[60%] lg:-translate-y-[20%] lg:blur-[150px]" />
+
+      <div className="absolute inset-0 z-[5] flex items-center justify-center mix-blend-plus-lighter overflow-hidden">
+        <div className="absolute top-[60px] sm:top-[40px] left-1/2 z-[2] h-[1000px] w-[1200px] md:h-full md:w-full md:scale-125 md:top-1/2 max-w-[1400px] -translate-x-1/2 md:-translate-y-2/5 xxl:scale-100">
+          <video src="/animSquare.mp4" autoPlay muted loop className="h-full w-full  xxl:object-cover rotate-90 md:rotate-0" />
+        </div>
+      </div>
+
+      <div className="[&>div>div:first-child>div]:text-textBlue flex justify-center [&>div>div:first-child>div]:bg-white [&>div>div:first-child>h3]:text-black">
+        <CardsCareer cardCareer={cardCareer} />
+      </div>
+
+    </div>
+  );
 };
