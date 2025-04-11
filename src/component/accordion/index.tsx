850083d78d80124ee2b011b507e50e918276372a
VladDot <107644242+VladDot@users.noreply.github.com>
2025-04-11 13:08:10 +0000

    fix arrow (#34)

--- src/component/accordion/index.tsx
+++ src/component/accordion/index.tsx
@@ -1,51 +1,31 @@
-import { getStyles } from "./styles";
-import { IAccordionProps } from "./types";
+import { Arrow } from '../arrow';
+import { getStyles } from './styles';
+import { IAccordionProps } from './types';
 
-export const Accordion = ({
-    title,
-    isOpen,
-    onClick,
-    description,
-}: IAccordionProps) => {
-    const styles = getStyles({ isOpen });
+export const Accordion = ({ title, isOpen, onClick, description }: IAccordionProps) => {
+  const styles = getStyles({ isOpen });
 
-    return (
-        <div className="w-full bg-white ">
-            <div
-                onClick={() => onClick()}
-                className={styles.header}
-            >
-                <h3 className="text-lg font-semibold md:text-[20px]">
-                    {title}
-                </h3>
+  return (
+    <div className="w-full bg-white">
+      <div onClick={() => onClick()} className={styles.header}>
+        <h3 className="text-lg font-semibold md:text-[20px]">{title}</h3>
 
-                <div className={styles.arrowWrapper}>
-                    <span
-                        className={`${styles.arrow} ${
-                            isOpen ? "rotate-180" : ""
-                        }`}
-                    >
-                        ▼
-                    </span>
-                </div>
-            </div>
-
-            <div
-                className={`${styles.content} ${
-                    isOpen ? "max-h-96 opacity-100" : "max-h-0 opacity-0 "
-                }`}
-            >
-                <ul className="space-y-1 list-disc pl-6 marker:text-textBlue ">
-                    {description.map((e, idx) => (
-                        <li
-                            className="py-1"
-                            key={`${e}_${idx}`}
-                        >
-                            {e}
-                        </li>
-                    ))}
-                </ul>
-            </div>
+        <div className={styles.arrowWrapper}>
+          <span className={`${styles.arrow} ${isOpen ? 'rotate-270' : ''}`}>
+            <Arrow className="" />
+          </span>
         </div>
-    );
+      </div>
+
+      <div className={`${styles.content} ${isOpen ? 'max-h-96 opacity-100' : 'max-h-0 opacity-0'}`}>
+        <ul className="marker:text-textBlue list-disc space-y-1 pl-6">
+          {description.map((e, idx) => (
+            <li className="py-1" key={`${e}_${idx}`}>
+              {e}
+            </li>
+          ))}
+        </ul>
+      </div>
+    </div>
+  );
 };
--- src/component/accordion/styles.ts
+++ src/component/accordion/styles.ts
@@ -1,15 +1,16 @@
-import { cn } from "@/utilities";
+import { cn } from '@/utilities';
 
 interface IGetStyles {
-    isOpen: boolean;
+  isOpen: boolean;
 }
 
 export const getStyles = ({ isOpen }: IGetStyles) => ({
-    header: "flex justify-between items-center border border-blueBorder/20 rounded-lg p-2 md:p-4 cursor-pointer bg-white shadow-md transition-all duration-300 relative z-[4]",
-    arrowWrapper: "transition-transform",
-    arrow: "inline-block transition-transform duration-300",
-    content: cn(
-        "overflow-hidden transition-all duration-300 max-h-0 opacity-0 bg-transparent ",
-        isOpen && "max-h-96 opacity-100"
-    ),
+  header:
+    'flex justify-between items-center border relative z-[6] border-blueBorder/20 rounded-lg p-2 md:p-4 cursor-pointer bg-white shadow-md transition-all duration-300 relative z-[4]',
+  arrowWrapper: 'transition-transform',
+  arrow: 'inline-block transition-transform duration-300 rotate-90',
+  content: cn(
+    'overflow-hidden transition-all duration-300 max-h-0 opacity-0 bg-transparent ',
+    isOpen && 'max-h-96 opacity-100'
+  ),
 });
