d167bfa82e3ad19690a2cfe378d273285922e5fb
vlad <vladkostirya4@gmail.com>
2025-04-14 00:12:00 +0300

    fix title

--- src/component/career-development/index.tsx
+++ src/component/career-development/index.tsx
@@ -5,7 +5,7 @@ import { CardsCareer } from './cards-career';
 export const CareerDevelopment = () => {
   return (
     <div className="xxl:mb-[130px] relative mb-20 md:mb-[100px]">
-      <div className="m-auto mb-[30px] max-w-[650px] text-center md:mb-[60px]">
+      <div className="m-auto mb-[30px] max-w-[300px] text-center sm:max-w-[650px] md:mb-[60px]">
         <Title
           text="Як розвивається Corezoid-спеціаліст"
           className="leading-[100%] lg:max-w-[500px]"
