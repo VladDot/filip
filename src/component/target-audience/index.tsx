d2ae777a72981a10ac30c3a9143bf1a2c59a242c
VladDot <107644242+VladDot@users.noreply.github.com>
2025-04-16 13:54:55 +0000

    added text remove subtitle (#4)

--- src/component/target-audience/index.tsx
+++ src/component/target-audience/index.tsx
@@ -6,7 +6,7 @@ export const TargetAudience = () => {
   return (
     <section className="w-max-[1320px] xxl:mx-auto mb-16 bg-white px-5 pt-6 lg:mb-40 xl:pr-12 xl:pl-[60px]">
       <div className="m-auto mb-[30px] max-w-[650px] text-center md:mb-[60px]">
-        <Title text="Для кого будуть корисні наші курси?" className="leading-[100%]" />
+        <Title text="Для кого буде корисний курс?" className="leading-[100%]" />
       </div>
 
       <div className="xxl:[&>div>div:last-child]:border-r-0 xxl:[&>div>div:last-child]:border-b-0 flex justify-center [&>div>div:last-child]:border-b-0 md:[&>div>div:last-child]:border-b">
