b296c1c19b5084c6628c0b5e5590d4305b3f3bab
ASGladchenko <asgladchenko@gmail.com>
2025-05-30 23:34:11 +0300

    delete unused video

--- src/component/banner/index.tsx
+++ src/component/banner/index.tsx
@@ -7,52 +7,17 @@ import './style.css';
 
 export const Banner = () => {
   return (
-    <>
-      <section className="relative z-[2] flex h-[650px] w-full items-center justify-center overflow-hidden md:h-[808px]">
-        <div className="bgg absolute inset-0 z-[2]" />
+    <section className="relative z-[2] flex h-[650px] w-full items-center justify-center overflow-hidden md:h-[808px]">
+      <div className="bgg absolute inset-0 z-[2]" />
 
-        <div className="absolute top-3/5 right-0 z-[3] h-[70%] w-[80%] translate-x-[60%] -translate-y-[80%] rounded-full bg-[#027DFA80] blur-[140px]" />
-        <div className="absolute top-1/2 left-0 z-[3] h-[50%] w-[60%] -translate-x-1/2 -translate-y-1/2 rounded-full bg-[#027DFA80] blur-[140px]" />
+      <div className="absolute top-3/5 right-0 z-[3] h-[70%] w-[80%] translate-x-[60%] -translate-y-[80%] rounded-full bg-[#027DFA80] blur-[140px]" />
+      <div className="absolute top-1/2 left-0 z-[3] h-[50%] w-[60%] -translate-x-1/2 -translate-y-1/2 rounded-full bg-[#027DFA80] blur-[140px]" />
 
-        <div className="absolute bottom-0 left-0 z-[2] h-[20%] w-full translate-y-1/2 rounded-full bg-white blur-[40px]" />
-        <div className="absolute top-0 left-0 z-[2] h-[25%] w-full -translate-y-1/2 rounded-full bg-white blur-[40px]" />
+      <div className="absolute bottom-0 left-0 z-[2] h-[20%] w-full translate-y-1/2 rounded-full bg-white blur-[40px]" />
+      <div className="absolute top-0 left-0 z-[2] h-[25%] w-full -translate-y-1/2 rounded-full bg-white blur-[40px]" />
 
-        <div className="pointer-events-none absolute inset-0 z-[5] flex items-center justify-center mix-blend-plus-lighter select-none">
-          <div className="pointer-events-none absolute top-0 z-[2] w-full rotate-180 select-none">
-            <video
-              loop
-              muted
-              autoPlay
-              playsInline
-              preload="auto"
-              src="/grid.mp4"
-              poster="/gridWebP.webp"
-              className="pointer-events-none h-[622px] w-full object-cover select-none md:h-[808px]"
-            />
-          </div>
-        </div>
-
-        <div className="relative z-[10] flex flex-col items-center gap-y-[10px] px-5 sm:max-w-[500px] lg:max-w-[700px] lg:gap-y-[30px] lg:px-0">
-          <h1 className="xxl:text-[56px] cursor-default text-center text-[clamp(36px,_4vw,_56px)] leading-[100%] font-semibold">
-            Опануй професію <br /> Corezoid-розробника <br /> з нуля
-          </h1>
-
-          <SubTitle
-            text="Відеоуроки, реальні приклади та практика допоможуть тобі навчитись створювати робочі процеси в Corezoid, працювати з API та використовувати JavaScript"
-            className="m-0 mb-5 text-center text-[clamp(18px,_4vw,_22px)] lg:max-w-[700px] lg:px-2"
-          />
-
-          <Button
-            text="Переглянути варіанти навчання"
-            className="w-fit gap-3"
-            rotate
-            onClick={() => scrollToSection('#learning_options')}
-          />
-        </div>
-      </section>
-      <div className="relative z-[1] bg-white">
-        <div className="absolute top-1/2 left-1/2 h-[300px] w-[300px] -translate-x-1/2 -translate-y-1/2 bg-blue-500"></div>
-        <div className="relative z-[1] mix-blend-plus-lighter">
+      <div className="pointer-events-none absolute inset-0 z-[5] flex items-center justify-center mix-blend-plus-lighter select-none">
+        <div className="pointer-events-none absolute top-0 z-[2] w-full rotate-180 select-none">
           <video
             loop
             muted
@@ -61,10 +26,28 @@ export const Banner = () => {
             preload="auto"
             src="/grid.mp4"
             poster="/gridWebP.webp"
-            className="pointer-events-none z-[999] h-[622px] w-full object-cover select-none md:h-[808px]"
+            className="pointer-events-none h-[622px] w-full object-cover select-none md:h-[808px]"
           />
         </div>
       </div>
-    </>
+
+      <div className="relative z-[10] flex flex-col items-center gap-y-[10px] px-5 sm:max-w-[500px] lg:max-w-[700px] lg:gap-y-[30px] lg:px-0">
+        <h1 className="xxl:text-[56px] cursor-default text-center text-[clamp(36px,_4vw,_56px)] leading-[100%] font-semibold">
+          Опануй професію <br /> Corezoid-розробника <br /> з нуля
+        </h1>
+
+        <SubTitle
+          text="Відеоуроки, реальні приклади та практика допоможуть тобі навчитись створювати робочі процеси в Corezoid, працювати з API та використовувати JavaScript"
+          className="m-0 mb-5 text-center text-[clamp(18px,_4vw,_22px)] lg:max-w-[700px] lg:px-2"
+        />
+
+        <Button
+          text="Переглянути варіанти навчання"
+          className="w-fit gap-3"
+          rotate
+          onClick={() => scrollToSection('#learning_options')}
+        />
+      </div>
+    </section>
   );
 };
